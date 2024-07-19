763d390c3a97cfd0d74d15dd9385314ec5541d6b
Gabriel <gabriel.moretto9090@gmail.com>
2024-07-19 11:40:13 -0300

    create user service test

--- src/user/application/services/test/create-user.service.spec.ts
+++ src/user/application/services/test/create-user.service.spec.ts
@@ -1,21 +1,57 @@
-import { Test, TestingModule } from "@nestjs/testing";
 import { CreateUserService } from "../create-user.service";
-import { EncryptPasswordServiceProvider } from "../../ports/out/encrypt-password.service";
-import { EncryptPasswordServiceImpl } from "src/user/infrastructure/adapters/out/encrypt-password";
+import { TestBed } from "@automock/jest";
 import { CreateUserCommand } from "../../ports/in/commands/create-user.command";
+import { UserRepositoryImpl } from "src/user/infrastructure/adapters/out/user.db";
+import { UserRepositoryProvider } from "../../ports/out/user.repository";
+import { User } from "src/user/domain/user.model";
+import { randomUUID } from "crypto";
+import { UserCredentials } from "src/user/domain/user-credentials.model";
+import { UserVisibilityConfig } from "src/user/domain/user-visibility-config.model";
+import { UserVisibility } from "src/user/domain/user-visibility.enum";
+import { UserAlreadyExistsError } from "../errors/user-already-exists.error";
+
+const command = new CreateUserCommand(
+    "Test123",
+    "test123@test.test",
+    "Password123"
+);
+
+const resolvedValue = () => {
+    const id = randomUUID();
+
+    return User.create(
+        id,
+        "Teste123",
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        UserCredentials.create(id, command.name, command.email, command.password, command.phoneNumber, null, null),
+        UserVisibilityConfig.create(
+            id,
+            UserVisibility.PUBLIC,
+            UserVisibility.PUBLIC,
+            UserVisibility.PUBLIC,
+            UserVisibility.PUBLIC,
+            UserVisibility.PUBLIC
+        ),
+        new Date(),
+        new Date(),
+        false
+    );
+};
 
 describe('CreateUserService', () => {
     let service: CreateUserService;
+    let repository: jest.Mocked<UserRepositoryImpl>
 
     beforeAll(async () => {
-        const fixture: TestingModule = await Test.createTestingModule({
-            providers: [
-                CreateUserService,
-                {provide: EncryptPasswordServiceProvider, useClass: EncryptPasswordServiceImpl}
-            ],
-        }).compile();
+        const { unit, unitRef } = TestBed.create(CreateUserService).compile();
 
-        service = fixture.get<CreateUserService>(CreateUserService);
+        service = unit;
+        repository = unitRef.get(UserRepositoryProvider);
     });
 
     it('should be defined', () => {
@@ -23,12 +59,30 @@ describe('CreateUserService', () => {
     });
 
     it('should create a user', async () => {
-        const user = await service.execute(new CreateUserCommand(
-            "Teste123",
-            "test@test.test",
-            "Senha@123"
-        ));
+        const id = randomUUID();
 
-        expect(user.first_name).toBe('Teste123');
+        repository.findByEmail.mockResolvedValue(null);
+        repository.findByName.mockResolvedValue(null);
+        repository.store.mockResolvedValue(resolvedValue());
+
+        const user = await service.execute(command);
+
+        expect(user.first_name).toBe('Test123');
+    });
+
+    it('should not create a user with the same email', async () => {
+        repository.findByEmail.mockResolvedValue(resolvedValue());
+
+        const user = await service.execute(command).catch(e => {
+            expect(e.constructor).toBe(UserAlreadyExistsError);
+        });
+    });
+
+    it('should not create a user with the same name', async () => {
+        repository.findByName.mockResolvedValue(resolvedValue());
+
+        const user = await service.execute(command).catch(e => {
+            expect(e.constructor).toBe(UserAlreadyExistsError);
+        });
     });
 });
